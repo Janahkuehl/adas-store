343b492c3fbe7b5c40cceca052c5e1add9fe2081
Janah Kuehl <janahs2@hotmail.com>
2024-08-08 20:00:33 -0300

    :construction: (feat) categoria: organizando o componente lista

--- frontend/store-front/src/app/app.module.ts
+++ frontend/store-front/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { CategoriaListaComponent } from './componentes/categoria/categoria-lista/categoria-lista.component';
 import { CategoriaFormComponent } from './componentes/categoria/categoria-form/categoria-form.component';
 import { CategoriaDetalheComponent } from './componentes/categoria/categoria-detalhe/categoria-detalhe.component';
+import { CabecalhoComponent } from './componentes/compartilhados/cabecalho/cabecalho.component';
+import { MenuComponent } from './componentes/compartilhados/menu/menu.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,10 +18,14 @@ import { CategoriaDetalheComponent } from './componentes/categoria/categoria-det
     CategoriaListaComponent,
     CategoriaFormComponent,
     CategoriaDetalheComponent,
+    CabecalhoComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
